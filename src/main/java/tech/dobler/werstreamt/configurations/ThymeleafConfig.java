9f0c8f9426daefa4098799adec22e446529ed497
Valerij Dobler <vd@wps.de>
2024-07-19 23:46:49 +0200

    Adds layout & navbar

---
+++ src/main/java/tech/dobler/werstreamt/configurations/ThymeleafConfig.java
@@ -0,0 +1,13 @@
+package tech.dobler.werstreamt.configurations;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ThymeleafConfig {
+	@Bean
+	public LayoutDialect layoutDialect() {
+		return new LayoutDialect();
+	}
+}
