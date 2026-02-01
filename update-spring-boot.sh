8ba4d0850408a8ce913bbc934ae9b4ddc1f76216
Valerij Dobler <vd@wps.de>
2026-02-01 16:26:05 +0100

    Commit with versions

--- update-spring-boot.sh
+++ update-spring-boot.sh
@@ -6,12 +6,26 @@ handle_error() {
   exit 1
 }
 
+get_spring_boot_version() {
+  awk '/<parent>/,/<\/parent>/ { if (/<version>/) { gsub(/<version>|<\/version>|[[:space:]]/, ""); print } }' pom.xml
+}
+
+## Update
+SPRING_BOOT_VERSION=$(get_spring_boot_version)
 echo "Updating parent version..."
 mvn versions:update-parent -DgenerateBackupPoms=false || handle_error
+NEW_SPRING_BOOT_VERSION=$(get_spring_boot_version)
+
+## Test
 echo "Running tests..."
 mvn clean package || handle_error
 
-echo "Update succeeded, committing and pushing..."
+## Checking in
+echo "Update to $NEW_SPRING_BOOT_VERSION succeeded, committing and pushing..."
+
 git add pom.xml
-git commit -m "Update sb"
-echo "Update complete"
+git commit -m "Update sb $SPRING_BOOT_VERSION -> $NEW_SPRING_BOOT_VERSION"
+git pull --rebase
+echo "pushing..."
+git push || handle_error
+echo "pushed"
