a375cbd98e6fd9d3ad6ce1d3b710d0d0b6980465
Valerij Dobler <vd@wps.de>
2026-02-01 16:16:36 +0100

    Handle errors

--- update-spring-boot.sh
+++ update-spring-boot.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-mvn versions:update-parent -DgenerateBackupPoms=false
-mvn clean package
-if [[ $? ]]; then
-  echo "Update failed, reverting";
-  git reset --hard
-  exit 1;
-fi
+handle_error() {
+  echo "Error occurred. Resetting changes..."
+  git reset --hard "$CURRENT_HEAD"
+  exit 1
+}
+
+echo "Updating parent version..."
+mvn versions:update-parent -DgenerateBackupPoms=false || handle_error
+echo "Running tests..."
+mvn clean package || handle_error
 
 echo "Update succeeded, committing and pushing..."
 git add pom.xml
 git commit -m "Update sb"
+echo "Update complete"
