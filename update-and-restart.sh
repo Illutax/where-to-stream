483611834bfa3a188870fcf40abc1ba7053daa9a
Valerij Dobler <vd@wps.de>
2026-07-26 20:30:21 +0200

    Fix ammend

--- update-and-restart.sh
+++ update-and-restart.sh
@@ -28,7 +28,7 @@ if [ -z "$GIT_COMMIT_HASH" ]; then
 fi
 
 DOCKER_IMAGE_TAG="$(git show -s --format=%cs)_$GIT_COMMIT_HASH"
-echo "DOCKER_IMAGE_TAG=$DOCKER_IMAGE_TAG" > .env
+echo "DOCKER_IMAGE_TAG=$DOCKER_IMAGE_TAG" >> .env
 docker build . --build-arg "DOCKER_IMAGE_TAG=$DOCKER_IMAGE_TAG" -t "w2s:$DOCKER_IMAGE_TAG"
 docker compose --env-file .env up -d
 docker compose logs -f
